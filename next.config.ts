cfa3b2ce9ba8e239e875737cf1aeaffded72ebaa
FlecorTLG <troels@flecor.com>
2025-12-28 00:33:24 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  i18n: {
-    locales: ["da", "en"],
-    defaultLocale: "da",
-  },
 };
 
 export default nextConfig;
